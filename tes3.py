8adf101ae6fa01fcadddf5b7265296ed60cd95b9
Diplan12 <rafaeldiplansuazo@gmail.com>
2024-05-06 20:33:56 -0400

    test 3 descuentos de barra

---
+++ tes3.py
@@ -0,0 +1,32 @@
+import shlex
+import sys
+
+def descuento (valor):
+    desc=0.60
+    total = valor *desc
+    return total
+
+
+
+
+
+def main():
+    """Echo the input arguments to standard output"""
+    precioBarra=3.49
+    x = float(input("Barras vendidas que no son del dia:""\n"))
+    print("----------------------------")
+    print("El precio estandar es de : "+ str(precioBarra))
+    print("El descuento aplicado será: "+ str(descuento(x)))
+    print("El precio total es de : "+str((x*precioBarra)-descuento(x)))
+    print("----------------------------")    
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main()) 
+
+
--- test 2.py
+++ test 2.py
@@ -6,5 +6,3 @@ print("El pago que le coresponde es",(horas*costo))
 n = int(input("Introduce un número entero: "))
 suma = n * (n + 1) / 2
 print("La suma de los primeros números enteros desde 1 hasta " + str(n) + " es " + str(suma))
-
-
