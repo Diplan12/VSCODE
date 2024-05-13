5cfb43ad041afe80993e7c23f2bbd2fb05c717a1
Diplan12 <rafaeldiplansuazo@gmail.com>
2024-05-13 12:05:22 -0400

    funciones

---
+++ funciones.py
@@ -0,0 +1,93 @@
+import sys
+import math
+
+
+"""
+#EJERCICIO #1
+def nompre():
+    print("HOLA AMIGA!")
+
+def nompref(nombre):
+    print("HOLA "+nombre)
+
+
+    
+    HACERLE COPIA A TODAS LAS LISTA 
+    HACERLE UNA LISTA PARA PONERLE SUS ITBIS 
+    QUE SOLO TENGA SKU NOMBRE CATEGORIA Y EL IMPUESTO 
+    REALIZAR PARA EL ITBIS 
+
+    CATEGORIA Y SUBFAMILIA: 
+
+    MANDAR LISTA CARLOS REYES Y A MARTIN 
+
+
+
+#2
+def nompref(nombre):
+    factorial=1
+    for factor in range(nombre):
+        factorial *= factor+1
+
+"""
+
+
+"""Escribir una función que calcule el área de un círculo y
+ otra que calcule el volumen de un cilindro usando la primera función.
+ def circle_area(radius):
+    Función que calcula el area de un círculo.
+    Parámetros
+    radius: Es el radio del círculo.
+    Devuelve el área del círculo de radio radius. 
+    
+    pi = 3.1415
+    return pi*radius**2
+
+def cilinder_volume(radius, high):
+    Función que calcula el volumen de un cilindro.
+    Parámetros
+    radius: Es el radio de la base del cilindro.
+    high: Es la altura del cilindro.
+    Devuelve el volumen del clindro de radio radius y altura high.
+    
+    return circle_area(radius)*high
+
+print(cilinder_volume(3,5))
+ """
+
+
+"""
+#3
+def media(lista):
+    numerador= sum(lista)
+    denominador=len(lista)
+
+    return numerador/denominador
+
+
+def main():
+    numero = [1, 2, 3, 4, 5]
+    temp=0
+    
+    print(media(numero))"""
+
+
+
+"""#4
+def square(sample):
+    Función que calcula los cuadrados de una lista de números.
+    Parámetros
+    sample: Es una lista de números
+    Devuelve una lista con los cuadrados de los números de la lista sample.
+    
+    list = []
+    for i in sample:
+        list.append(i**2)
+    return list"""
+
+
+"""Escribir una función que calcule el máximo común divisor de dos números
+ y otra que calcule el mínimo común múltiplo."""
+
+
+
