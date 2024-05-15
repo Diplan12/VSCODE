e51efa592ee5b1085b606bb6ea5a41cc099ae7ec
Diplan12 <rafaeldiplansuazo@gmail.com>
2024-05-15 10:41:37 -0400

    depuracion de datos

---
+++ depuracion.py
@@ -0,0 +1,40 @@
+"""contraseña = input("Introduce la contraseña: ")
+
+if contraseña in ('sesamo'):
+  print('Pasa')
+
+else:
+  print('No pasa')"""
+
+
+
+"""def aplica_iva(base, iva = 21):
+    base = base * iva   
+    return base
+
+
+base = input('Introduce la base imponible de la factura: ')
+print(aplica_iva(base))"""
+
+
+"""u = [1, 2, 3]
+v = [4, 5, 6]
+
+def producto_escalar(u, v):
+    for i in range(len(u)):
+        u[i] *= v[i]
+    return sum(u)
+
+print(producto_escalar(u, v))"""
+
+"""listin = {'Juan':123456789, 'Pedro':987654321}
+
+def elimina(listin, usuario):
+    return listin.pop(usuario,"")
+
+elimina(listin,"Pedro")
+#listin.pop("Pedro")
+#print(listin)"""
+
+
+
